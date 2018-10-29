0e9e154b75b115a4ba628ae238b056982c5065f2
Riaan de Klerk <riaan.deklerk@fluxfederation.com>
2018-10-29 13:33:14 +1300

    convert guest user into actual users when loggin in for the first time

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,17 +7,27 @@ class User < ApplicationRecord
   scope :paid_game, ->(game) { payments.where.in(game.payments) }#joins(:payments).where(payments.game_id = game.id AND IN payments }
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
-      
-      #user.player = player
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.email = auth.info.email
-      user.name = auth.info.name
-      user.oauth_token = auth.credentials.token
-      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-      # TODO: include Google image here (auth.info.image)
-      user.save!
+    if where(provider: nil, uid: nil, email: auth.info.email).present?
+      # convert guest users into actual users
+      find_by(email: auth.info.email).only.tap do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.name = auth.info.name
+        user.oauth_token = auth.credentials.token
+        user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+        user.update!
+      end
+    else
+      where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.email = auth.info.email
+        user.name = auth.info.name
+        user.oauth_token = auth.credentials.token
+        user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+        # TODO: include Google image here (auth.info.image)
+        user.save!
+      end
     end
   end
 
