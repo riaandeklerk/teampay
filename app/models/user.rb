ef757a50a3a3caffea7c40ad1c5c029a68bb30b1
Riaan de Klerk <riaan.deklerk@fluxfederation.com>
2018-10-29 13:56:03 +1300

    refactor response for guest user

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,14 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     if where(provider: nil, uid: nil, email: auth.info.email).present?
       # convert guest users into actual users
-      find_by(email: auth.info.email).update!(
+      user = find_by(email: auth.info.email).update!(
         provider: auth.provider,
         uid: auth.uid,
         name: auth.info.name,
         oauth_token: auth.credentials.token,
         oauth_expires_at: Time.at(auth.credentials.expires_at)
       )
+      user
     else
       where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
         user.provider = auth.provider
